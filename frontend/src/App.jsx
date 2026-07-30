66e9bb20a8c1402b63c796b8d7a1e077ef3beb22
ishanyadav-hash <ishanyadav2707@gmail.com>
2026-07-31 01:13:14 +0530

    Small errors removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
