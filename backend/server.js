09198434deb0c66f9ca07c26e806febd2163ac1c
ishanyadav-hash <ishanyadav2707@gmail.com>
2026-07-30 23:43:35 +0530

    Added create new user and login user also fixed some error

--- backend/server.js
+++ backend/server.js
@@ -22,18 +22,19 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/users', async(req,res)=>{
-    const getUsersQuery=`
-    SELECT * FROM demo;`
-
     try{
+        const getUsersQuery=`
+        SELECT * FROM demo;`
+
         const result= await db.query(getUsersQuery);
-        res.send(200).json({
+
+        res.status(200).json({
             status: "Success",
             message: "All users fetched",
             data: result.rows
         })
     } catch(error){
-        return res.send(500).json({
+        return res.status(500).json({
             status: "Failed",
             message:"Something went wrong",
             error: error
@@ -41,6 +42,54 @@ app.get('/users', async(req,res)=>{
     }
 })
 
+app.post('/users', async(req,res)=>{
+    const {name,regd_no,email,password,age}=req.body
+
+    try{
+        const newUserQuery=`
+        INSERT INTO demo(name,regd_no,email,password,age)
+        VALUES ($1,$2,$3,$4,$5)
+        RETURNING id,name,regd_no,email,password,age`;
+
+        const result= await db.query(newUserQuery,[name,regd_no,email,password,age]);
+
+        res.status(201).json({
+            status: "Success",
+            message: "Created",
+            data: result.rows[0]
+        })
+    } catch(error){
+        return res.status(500).json({
+            status: "Failed",
+            message:"Something went wrong",
+            error: error
+        })
+    }
+})
+
+app.post('/login', async(req,res)=>{
+    const {name,password}=req.body
+
+    try{
+        const findUserQuery=`
+        SELECT * FROM demo
+        WHERE name=$1 AND password=$2`;
+
+        const result= await db.query(findUserQuery,[name,password]);
+
+        res.status(200).json({
+            status: "Success",
+            message: "Login Successful",
+            data: result.rows[0]
+        })
+    } catch(error){
+        return res.status(500).json({
+            status: "Failed",
+            message:"Something went wrong",
+            error: error
+        })
+    }
+})
 
 
 
